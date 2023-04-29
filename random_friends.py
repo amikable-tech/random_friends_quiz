a5461d9737900d5f214b4c80ff70bbbfd47b2ade
Chiamaka Adilieme <amikable97@gmail.com>
2023-04-29 15:24:08 +0200

    3rd change

--- random_friends.py
+++ random_friends.py
@@ -1,16 +1,14 @@
 import random
 num_friends = random.randint(2,10)
 each_friends_cash = random.randint(10,75) #money each friend can afford to pay 
-#total_bill = random.randint(200, 300)
 total_bill = (num_friends * each_friends_cash)
-friends_bill = [random.randint(5, 100) for _ in range(each_friends_cash)]
+#picking a random amount of money from the friends regardless of how much is in their pockets
+friends_bill = [random.randint(5, 100) for _ in range(each_friends_cash)] 
 avg_bill = total_bill//num_friends
 
 #check if any friend paid to little or too much
 bill_difference = [bill - avg_bill for bill in friends_bill]
- 
 #next
-
 for i in range(num_friends):
     if bill_difference [i] > 15:
         print(f'friend {i+1} paid €{bill_difference[i]:.2f} paid too much.')
